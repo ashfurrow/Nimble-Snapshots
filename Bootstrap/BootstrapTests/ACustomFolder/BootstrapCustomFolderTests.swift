02af2a79362866b92e07dbbda0531bf1fab780c1
Esteban Torres <me@estebantorr.es>
2015-10-30 17:24:42 -0600

    Tests for a custom `test folder` name.

---
+++ Bootstrap/BootstrapTests/ACustomFolder/BootstrapCustomFolderTests.swift
@@ -0,0 +1,29 @@
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+class BootstrapCustomFormatTests: QuickSpec {
+    override func spec() {
+        describe("in some context", { () -> () in
+            var view: UIView!
+            setNimbleTestFolder("CustomFolder")
+
+            beforeEach {
+                view = UIView(frame: CGRect(origin: CGPointZero, size: CGSize(width: 44, height: 44)))
+                view.backgroundColor = UIColor.blueColor()
+            }
+
+            it("fails to find the snapshots due to the custom folder") {
+                expect(view).notTo(haveValidSnapshot(named: "something custom"))
+            }
+
+            it("finds the snapshots using a custom images directory") {
+                expect(view).to(haveValidSnapshot())
+            }
+          
+            it("finds device agnostic snapshots with custom images directory") {
+                expect(view).to(haveValidDeviceAgnosticSnapshot())
+            }
+        })
+    }
+}
