dff36faa9ec45c53e4a3cc0e43487ce459d9106b
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-09-29 15:34:47 -0300

    Running tests on iOS 10 and 9

--- Bootstrap/Bootstrap/AppDelegate.swift
+++ Bootstrap/Bootstrap/AppDelegate.swift
@@ -18,8 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // There are device specific tests, so we need to be tight on the devices running tests
 
         let version = NSProcessInfo.processInfo().operatingSystemVersion
-        assert(version.majorVersion == 9 && version.minorVersion == 3 ,
-                 "The tests should be run on iOS 9.x, not \(version.majorVersion), \(version.minorVersion)");
+        let minVersion = NSOperatingSystemVersion(majorVersion: 9, minorVersion: 3, patchVersion: 0)
+        assert(NSProcessInfo.processInfo().isOperatingSystemAtLeastVersion(minVersion),
+                 "The tests should be run at least on iOS 9.3, not \(version.majorVersion), \(version.minorVersion)");
 
         let nativeResolution = UIScreen.mainScreen().nativeBounds.size;
         assert(UIDevice.currentDevice().userInterfaceIdiom == .Phone && CGSizeEqualToSize(nativeResolution, CGSizeMake(640, 960)),
