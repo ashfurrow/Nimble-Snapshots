630afb9d05ef6740dbb1d7e04dcd0f6b591cb511
joaolfp <joaolucasfp2001@gmail.com>
2023-09-23 10:32:34 -0300

    Setup project example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let photo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "photo")
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(photo)
+        
+        photo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        photo.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        photo.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        photo.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        
+        view.backgroundColor = .white
     }
 
-
 }
-
