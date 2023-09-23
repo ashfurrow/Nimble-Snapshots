6f563b1758d8be79ab1539db2494da3ac94132d4
joaolfp <joaolucasfp2001@gmail.com>
2023-09-23 09:52:31 -0300

    Create project example

--- Bootstrap/Pods/Target Support Files/Pods-Bootstrap/Pods-Bootstrap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bootstrap : NSObject
-@end
-@implementation PodsDummy_Pods_Bootstrap
-@end
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by João Lucas on 23/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
